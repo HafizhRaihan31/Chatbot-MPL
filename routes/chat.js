99c940328be944dceb31624af7d4f09047812f2e
Hafizh Raihan <hafizhr205@gmail.com>
2025-12-09 12:50:57 +0700

    ubah model ai lagi

--- routes/chat.js
+++ routes/chat.js
@@ -22,7 +22,7 @@ const roster = loadJSON("teams_detail.json");
 
 // SETUP GEMINI (HEMAT TOKEN)
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-8b" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
 
 
 // ------------------------------------------------------------------
